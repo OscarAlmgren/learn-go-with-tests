9e51b62a25c68cb626d79f6a4ed855f9f4cedcdb
Oscar Almgren <oscar.almgren@gmail.com>
2024-02-20 10:05:11 +0100

    Added passing tests for Perimeter, Area. Also added Shape interface - needs Area()

--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -1,1 +1,34 @@
 package structsmethodsinterfaces
+
+import "testing"
+
+type Shape interface {
+	Area() float64
+}
+
+// type Square struct {
+// 	width  float64
+// 	height float64
+// }
+
+// type Circle struct {
+// 	radius float64
+// }
+
+func TestPerimeter(t *testing.T) {
+	got := Perimeter(10.0, 10.0)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.f, want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	got := Area(10.0, 10.0)
+	want := 100.0
+
+	if got != want {
+		t.Errorf("got %.f, want %.2f", got, want)
+	}
+}
