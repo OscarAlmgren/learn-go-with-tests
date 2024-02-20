6de43895f454b41c526369720b8c98d8d22a4fe1
Oscar Almgren <oscar.almgren@gmail.com>
2024-02-20 10:03:36 +0100

    Add Perimeter and Area funcs

--- structs-methods-interfaces/shapes.go
+++ structs-methods-interfaces/shapes.go
@@ -1,1 +1,11 @@
 package structsmethodsinterfaces
+
+func Perimeter(width, height float64) (perimeter float64) {
+	perimeter = 2 * (width + height)
+	return
+}
+
+func Area(width, height float64) (area float64) {
+	area = width * height
+	return
+}
