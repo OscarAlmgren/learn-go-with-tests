26c6bf9f1b247214055417f3d1a590074f0c806e
Oscar Almgren <oscar.almgren@gmail.com>
2024-02-20 15:02:15 +0100

    Add wallet with Deposit and Balance reciever methods. Also, add type Bitcoin based on int and then String() for domain specific %s in printf output

--- pointers-errors/wallet.go
+++ pointers-errors/wallet.go
@@ -1,1 +1,21 @@
 package pointerserrors
+
+import "fmt"
+
+type Bitcoin int
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() (balance Bitcoin) {
+	balance = w.balance
+	return
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
