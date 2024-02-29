3298be58a942c1d52d9cb0fcf06bc9a42eadc101
Oscar Almgren <oscar.almgren@gmail.com>
2024-02-29 09:42:57 +0100

    Updated Withdraw to throw error if insufficient funds.

--- pointers-errors/wallet.go
+++ pointers-errors/wallet.go
@@ -1,6 +1,9 @@
 package pointerserrors
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 type Wallet struct {
@@ -9,6 +12,14 @@ type Wallet struct {
 
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
+	w.balance -= amount
+	return nil
 }
 
 func (w *Wallet) Balance() (balance Bitcoin) {
