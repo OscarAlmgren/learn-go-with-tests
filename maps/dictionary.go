b3b636dd66306c6474f5bf279258e37832190bc4
Oscar Almgren <oscar.almgren@gmail.com>
2024-03-05 19:12:52 +0100

    Dictionary Add word and check if exists or not

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -4,11 +4,24 @@ import "errors"
 
 type Dictionary map[string]string
 
+var ErrNotFound = errors.New("could not find the word you were looking for")
+var ErrAlreadyFound = errors.New("word already exists in map")
+
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
 
 	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, ok := d[word]
+	if !ok {
+		d[word] = definition
+	} else {
+		return ErrAlreadyFound
+	}
+	return nil
 }
