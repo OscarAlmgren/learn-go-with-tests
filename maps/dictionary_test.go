03bdc292d4d6f38eac7dc172b4698528692bc4cd
Oscar Almgren <oscar.almgren@gmail.com>
2024-03-05 07:34:21 +0100

    Test case for search not in dictionary.
    Test cases for Add and Already Exists in dict

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -15,14 +15,32 @@ func TestSearch(t *testing.T) {
 
 	t.Run("not in dictionary", func(t *testing.T) {
 
-		_, err := dictionary.Search("not here")
-		want := "could not find the word you were looking for"
+		_, got := dictionary.Search("not here")
 
-		if err == nil {
+		if got == nil {
 			t.Fatal("expected to get an error.")
 		}
 
-		assertStrings(t, err.Error(), want)
+		assertError(t, got, ErrNotFound)
+	})
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+
+	t.Run("add word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		err := dictionary.Add(word, definition)
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("word exists already", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		err := dictionary.Add(word, definition)
+		assertError(t, err, ErrAlreadyFound)
 	})
 }
 
@@ -32,4 +50,24 @@ func assertStrings(t testing.TB, got, want string) {
 	if got != want {
 		t.Errorf("got %q want %q given, %q", got, want, "test")
 	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	assertStrings(t, got, definition)
 }
